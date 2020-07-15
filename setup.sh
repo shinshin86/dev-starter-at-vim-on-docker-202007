9de46b4d60633ae5c9da17169b46ddc2c5c3e3bd
shinshin86 <beagles1986@gmail.com>
2020-07-16 07:19:23 +0900

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -26,4 +26,20 @@ add-apt-repository ppa:longsleep/golang-backports
 apt update
 apt install -y golang-go
 
+# pyenv install
+apt install -y build-essential libffi-dev libssl-dev zlib1g-dev liblzma-dev libbz2-dev libreadline-dev libsqlite3-dev
+git clone https://github.com/pyenv/pyenv.git ~/.pyenv
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
+echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(pyenv init -)"' >> ~/.bashrc
+
+# Install rbenv
+apt install -y autoconf bison libyaml-dev libreadline6-dev libncurses5-dev libgdbm5 libgdbm-dev
+git clone https://github.com/rbenv/rbenv.git ~/.rbenv
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
+~/.rbenv/bin/rbenv init
+echo 'eval "$(rbenv init -)"' >> ~/.bashrc
+mkdir -p "$(rbenv root)"/plugins
+git clone https://github.com/rbenv/ruby-build.git "$(rbenv root)"/plugins/ruby-build
+
 source ~/.bashrc
